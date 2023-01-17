406aa265cebd94d24e4a40de8bee9acb35e0576f
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-01-17 17:42:48 +0100

    init

---
+++ src/main/java/org/pablomartin/S5T2Dice_Game/S5T2DiceGameApplication.java
@@ -0,0 +1,13 @@
+package org.pablomartin.S5T2Dice_Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5T2DiceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5T2DiceGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pablomartin/S5T2Dice_Game/S5T2DiceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pablomartin.S5T2Dice_Game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5T2DiceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
