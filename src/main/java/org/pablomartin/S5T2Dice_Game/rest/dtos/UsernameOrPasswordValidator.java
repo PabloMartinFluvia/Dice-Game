48f04a1d3fe9eca57299d7945b1c018cf858b528
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-01-28 15:54:14 +0100

    username or password validation

--- src/main/java/org/pablomartin/S5T2Dice_Game/rest/dtos/UsernameOrPasswordValidator.java
+++
@@ -1,6 +0,0 @@
-package org.pablomartin.S5T2Dice_Game.rest.dtos;
-
-import jakarta.validation.ConstraintValidator;
-
-public class UsernameOrPasswordValidator implements ConstraintValidator {
-}
--- src/main/java/org/pablomartin/S5T2Dice_Game/rest/dtos/validations/UsernameOrPassword.java
+++ src/main/java/org/pablomartin/S5T2Dice_Game/rest/dtos/validations/UsernameOrPassword.java
@@ -2,8 +2,6 @@ package org.pablomartin.S5T2Dice_Game.rest.dtos.validations;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import org.pablomartin.S5T2Dice_Game.rest.dtos.UsernameOrPasswordValidator;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy= UsernameOrPasswordValidator.class)
+@Constraint(validatedBy= org.pablomartin.S5T2Dice_Game.rest.dtos.validations.UsernameOrPasswordValidator.class)
 public @interface UsernameOrPassword {
     String message() default "{credentials.update}";
     Class<?>[] groups() default {};
