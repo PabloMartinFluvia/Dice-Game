431360203644c362a45fe5caa9cf4ad286bf20d1
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-01-28 15:51:45 +0100

    step 2

--- src/main/java/org/pablomartin/S5T2Dice_Game/rest/dtos/UsernameOrPassword.java
+++ src/main/java/org/pablomartin/S5T2Dice_Game/rest/dtos/UsernameOrPassword.java
@@ -1,4 +1,19 @@
-package org.pablomartin.S5T2Dice_Game.rest.dtos;
+package org.pablomartin.S5T2Dice_Game.rest.dtos.validations;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import org.pablomartin.S5T2Dice_Game.rest.dtos.UsernameOrPasswordValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy= UsernameOrPasswordValidator.class)
 public @interface UsernameOrPassword {
+    String message() default "{credentials.update}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
---
+++ src/main/java/org/pablomartin/S5T2Dice_Game/rest/dtos/UsernameOrPasswordValidator.java
@@ -0,0 +1,6 @@
+package org.pablomartin.S5T2Dice_Game.rest.dtos;
+
+import jakarta.validation.ConstraintValidator;
+
+public class UsernameOrPasswordValidator implements ConstraintValidator {
+}
